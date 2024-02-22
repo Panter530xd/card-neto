85759a4fd486356cf24bd122ebfc1ef4ffa5eaa7
Panter530xd <bobansenic@gmail.com>
2024-02-22 01:16:00 +0100

    fixed on mobile testimonial slider

--- src/components/common/Testimonial.tsx
+++ src/components/common/Testimonial.tsx
@@ -48,75 +48,77 @@ const TestimonialSlider: React.FC<TestimonialSliderProps> = ({
   };
 
   return (
-    <div className="relative flex items-center justify-center bg-black lg:py-14 py-8">
-      <div className="embla" ref={emblaRef}>
-        <div className="embla__container">
-          {testimonials.map((testimonial, index) => (
-            <div className="embla__slide" key={testimonial.id}>
-              <div
-                className={` md:w-10/12 mx-auto  ${
-                  index === selectedIndex ? '' : 'hidden'
-                }`}
-              >
-                <h2 className="text-[32px] text-pure-white font-bold text-center pb-2">
-                  {testimonial.title}
-                </h2>
-                <p className="text-pure-white text-lg lg:w-8/12 w-full mx-auto text-center">
-                  {testimonial.content}
-                </p>
-                <div className="flex justify-center items-center mt-2">
-                  {[...Array(5)].map((_, i) => (
-                    <svg
-                      key={i}
-                      className={`w-5 h-5 fill-current ${
-                        i < testimonial.stars
-                          ? 'text-yellow-500'
-                          : 'text-gray-300'
-                      }`}
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10 1l2.6 5.3L18 7.4l-4.1 4.1.9 5.3L10 15.4l-4.8 2.4.9-5.3L2 7.4l5.4-.1L10 1z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  ))}
-                </div>
-                <div className="flex justify-center items-center mt-4">
-                  <div className="w-12 h-12 rounded-full overflow-hidden mr-4">
-                    <Image
-                      src={testimonial.imageSrc}
-                      width={64}
-                      height={64}
-                      alt={`${testimonial.firstName} ${testimonial.lastName}`}
-                    />
+    <section className="relative flex items-center justify-center bg-black py-14">
+      <div className="embla">
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
+            {testimonials.map((testimonial, index) => (
+              <div className="embla__slide" key={testimonial.id}>
+                <div
+                  className={` md:w-10/12 mx-auto  ${
+                    index === selectedIndex ? '' : 'hidden'
+                  }`}
+                >
+                  <h2 className="text-[32px] text-pure-white font-bold text-center pb-2">
+                    {testimonial.title}
+                  </h2>
+                  <p className="text-pure-white text-lg lg:w-8/12 w-full mx-auto text-center">
+                    {testimonial.content}
+                  </p>
+                  <div className="flex justify-center items-center mt-2">
+                    {[...Array(5)].map((_, i) => (
+                      <svg
+                        key={i}
+                        className={`w-5 h-5 fill-current ${
+                          i < testimonial.stars
+                            ? 'text-yellow-500'
+                            : 'text-gray-300'
+                        }`}
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M10 1l2.6 5.3L18 7.4l-4.1 4.1.9 5.3L10 15.4l-4.8 2.4.9-5.3L2 7.4l5.4-.1L10 1z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    ))}
                   </div>
-                  <div>
-                    <p className="text-pure-white text text-xl font-medium">{`${testimonial.firstName} ${testimonial.lastName}`}</p>
-                    <p className="text-sm text-pure-white">
-                      {testimonial.businessDetails}
-                    </p>
+                  <div className="flex justify-center items-center mt-4">
+                    <div className="w-12 h-12 rounded-full overflow-hidden mr-4">
+                      <Image
+                        src={testimonial.imageSrc}
+                        width={64}
+                        height={64}
+                        alt={`${testimonial.firstName} ${testimonial.lastName}`}
+                      />
+                    </div>
+                    <div>
+                      <p className="text-pure-white text text-xl font-medium">{`${testimonial.firstName} ${testimonial.lastName}`}</p>
+                      <p className="text-sm text-pure-white">
+                        {testimonial.businessDetails}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            ))}
+          </div>
+        </div>
+        <div className="absolute bottom-6 left-0 right-0 flex justify-center space-x-2">
+          {testimonials.map((_, index) => (
+            <button
+              key={index}
+              className={`h-3 w-3 rounded-full ${
+                index === selectedIndex ? 'bg-pure-white' : 'bg-[#6C6C6C]'
+              }`}
+              onClick={() => scrollTo(index)}
+            />
           ))}
         </div>
       </div>
-      <div className="absolute bottom-6 left-0 right-0 flex justify-center space-x-2">
-        {testimonials.map((_, index) => (
-          <button
-            key={index}
-            className={`h-3 w-3 rounded-full ${
-              index === selectedIndex ? 'bg-pure-white' : 'bg-[#6C6C6C]'
-            }`}
-            onClick={() => scrollTo(index)}
-          />
-        ))}
-      </div>
-    </div>
+    </section>
   );
 };
 
